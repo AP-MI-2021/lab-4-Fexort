79e1296e5e2e6af950581cd6643546ad2aa40351
unknown <opreamatei82@ymail.com>
2021-10-20 19:12:44 +0300

    al 2 lea commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def citire_lista():
     lst = []
     n = int(input("Dati numarul de elemente din lista: "))
@@ -9,19 +8,29 @@ def citire_lista():
 def nrnegative(lst):
     for i in range(len(lst)):
         if lst[i] < 0:
-            print(" ", lst[i])
+            print(lst[i])
+def celmaimicnumarcitit(lst):
+    x = int(input("Dati o cifra"))
+    minx = 10000
+    for i in range(len(lst)):
+        if lst[i]%10 == x and lst[i] < minx:
+            minx = lst[i]
+    print(minx)
 
 
 def main():
     while True:
         print("1.Citire date")
         print("2.Afisarea numerelor negative din lista")
+        print("3.Afisarea celui mai mic numar care are ultima cifra egala cu o cifra citita de la tastatura")
         print("Pentru a inchide, introduce x")
         optiune = input("Dati optiunea: ")
         if optiune == "1":
             l = citire_lista()
         elif optiune == "2":
             nrnegative(l)
+        elif optiune == "3":
+            celmaimicnumarcitit(l)
         elif optiune == "x":
             break
 
