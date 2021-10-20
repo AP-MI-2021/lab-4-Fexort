55f05042c80a49f8c510b7a503ab79226d7e7dbd
unknown <opreamatei82@ymail.com>
2021-10-20 19:31:36 +0300

    al 3 lea commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def citire_lista():
+    '''
+    citeste lista
+    '''
     lst = []
     n = int(input("Dati numarul de elemente din lista: "))
     for i in range(n):
@@ -6,16 +9,42 @@ def citire_lista():
     return lst
 
 def nrnegative(lst):
+    '''
+    afiseaza numerele negative din lista
+    '''
     for i in range(len(lst)):
         if lst[i] < 0:
             print(lst[i])
 def celmaimicnumarcitit(lst):
+    '''
+    afiseaza cel mai mic numar care are ultima cifra egala cu cifra citita
+    '''
     x = int(input("Dati o cifra"))
     minx = 10000
     for i in range(len(lst)):
         if lst[i]%10 == x and lst[i] < minx:
             minx = lst[i]
     print(minx)
+def nrprim(x):
+    '''
+    determina daca numarul este prim
+    '''
+    if x<2:
+        return False
+    for i in range(2, x//2+1):
+        if x % i == 0:
+            return False
+    return True
+def superprim(lst):
+    '''
+    afiseaza numerele superprime
+    '''
+    for i in range(len(lst)):
+        n = lst[i]
+        while nrprim(n) == True and n != 0:
+            n = n // 10
+        if n == 0:
+            print(lst[i])
 
 
 def main():
@@ -23,6 +52,7 @@ def main():
         print("1.Citire date")
         print("2.Afisarea numerelor negative din lista")
         print("3.Afisarea celui mai mic numar care are ultima cifra egala cu o cifra citita de la tastatura")
+        print("4.Afisarea tuturor numerelor din lista care sunt superprime")
         print("Pentru a inchide, introduce x")
         optiune = input("Dati optiunea: ")
         if optiune == "1":
@@ -31,10 +61,11 @@ def main():
             nrnegative(l)
         elif optiune == "3":
             celmaimicnumarcitit(l)
+        elif optiune == "4":
+            superprim(l)
         elif optiune == "x":
             break
 
 
 if __name__ == '__main__':
-
     main()
